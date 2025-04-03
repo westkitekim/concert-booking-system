681d3fc6e2c0f7e9e1f28f9a0a1d5f42a82eb4e7
westkitekim <westkite418@gmail.com>
2025-04-04 06:13:52 +0900

    style: 미사용 import 제거

--- src/main/java/kr/hhplus/be/server/api/controller/ConcertController.java
+++ src/main/java/kr/hhplus/be/server/api/controller/ConcertController.java
@@ -3,7 +3,6 @@ package kr.hhplus.be.server.api.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.hhplus.be.server.api.controller.dto.response.SeatInfo;
-import kr.hhplus.be.server.api.controller.dto.response.SimpleErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
