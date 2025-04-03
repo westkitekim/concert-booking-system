40917067cc69edda3db57722409f8f6af65a014a
westkitekim <westkite418@gmail.com>
2025-04-04 06:04:10 +0900

    feat: 결제 Mock API, Fake DTO 구현

---
+++ src/main/java/kr/hhplus/be/server/api/controller/dto/response/PaymentResponse.java
@@ -0,0 +1,13 @@
+package kr.hhplus.be.server.api.controller.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record PaymentResponse(
+        @Schema(description = "결제 결과", example = "success")
+        String result,
+        @Schema(description = "좌석 소유자 ID", example = "user123")
+        String ownerUserId,
+        @Schema(description = "좌석 번호", example = "1")
+        String seatNumber
+) {}
+
