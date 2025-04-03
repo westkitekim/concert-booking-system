04561feacde6901d00c9ef075dfd2e23c04c4012
westkitekim <westkite418@gmail.com>
2025-04-04 06:13:19 +0900

    bug: concertId 입력감 검증 로직 추가

--- src/main/java/kr/hhplus/be/server/api/controller/ConcertController.java
+++ src/main/java/kr/hhplus/be/server/api/controller/ConcertController.java
@@ -3,6 +3,8 @@ package kr.hhplus.be.server.api.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.hhplus.be.server.api.controller.dto.response.SeatInfo;
+import kr.hhplus.be.server.api.controller.dto.response.SimpleErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,12 +22,20 @@ class ConcertController {
     @GetMapping("/{concertId}/available-dates")
     @Operation(summary = "예약 가능 날짜 조회", description = "예약 가능한 날짜 조회")
     public ResponseEntity<List<LocalDate>> getAvailableDates(@PathVariable String concertId) {
+
+        if (concertId == null || concertId.isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(List.of());
+        }
         return ResponseEntity.ok(List.of(LocalDate.now(), LocalDate.now().plusDays(1)));
     }
 
     @GetMapping("/{concertId}/available-seats")
     @Operation(summary = "예약 가능 좌석 조회", description = "예약 가능한 좌석 목록 반환")
     public ResponseEntity<List<SeatInfo>> getAvailableSeats(@PathVariable String concertId) {
+
+        if (concertId == null || concertId.isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(List.of());
+        }
         return ResponseEntity.ok(List.of(
                 new SeatInfo("1", true, null),
                 new SeatInfo("2", false, "user123")
