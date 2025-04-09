ea9d87f6c45f54eaf90389df9dfdaaac68171853
westkitekim <westkite418@gmail.com>
2025-04-10 01:17:21 +0900

    fix: Account 단위테스트 import 오류 수정

--- src/test/java/kr/hhplus/be/server/domain/account/AccountServiceTest.java
+++ src/test/java/kr/hhplus/be/server/domain/account/AccountServiceTest.java
@@ -12,6 +12,7 @@ import java.math.BigDecimal;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
