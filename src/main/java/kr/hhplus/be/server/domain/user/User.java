452df42514d08ad620879029811a7fabdfe29ec1
westkitekim <westkite418@gmail.com>
2025-04-10 01:20:29 +0900

    feat: User 도메인 구현

---
+++ src/main/java/kr/hhplus/be/server/domain/user/User.java
@@ -0,0 +1,17 @@
+package kr.hhplus.be.server.domain.user;
+
+import lombok.Getter;
+
+@Getter
+public class User {
+
+    private final long userId;
+    private final String username;
+    private final String queueToken;
+
+    public User(long userId, String username, String queueToken) {
+        this.userId = userId;
+        this.username = username;
+        this.queueToken = queueToken;
+    }
+}
