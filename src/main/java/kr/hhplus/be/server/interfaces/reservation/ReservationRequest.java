08f9440a4e8108afbf2bc47d093659e2313e7571
westkitekim <westkite418@gmail.com>
2025-04-11 09:45:18 +0900

    feat: 예약 컨트롤러 DTO

--- src/main/java/kr/hhplus/be/server/interfaces/reservation/ReservationRequest.java
+++ src/main/java/kr/hhplus/be/server/interfaces/reservation/ReservationRequest.java
@@ -4,16 +4,24 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record ReservationRequest(
+        @Schema(description = "토큰", example = "EKJKE-EKFJLE")
+        @NotBlank String token,
         @Schema(description = "유저 ID", example = "user123")
-        @NotBlank String userId,
+        @NotBlank Long userId,
         @Schema(description = "콘서트 ID", example = "concert1")
         @NotBlank String concertId,
         @Schema(description = "날짜", example = "2025-04-10")
         @NotNull LocalDate date,
         @Schema(description = "좌석 번호", example = "1")
-        @NotBlank String seatNumber
+        @NotBlank String seatNumber,
+        @Schema(description = "스케줄 ID", example = "1001")
+        @NotNull Long scheduleId,
+        @Schema(description = "좌석 ID", example = "2001")
+        @NotNull Long seatId,
+        @Schema(description = "결제 금액", example = "10000")
+        @NotNull BigDecimal amount
 ) {}
-
--- src/main/java/kr/hhplus/be/server/interfaces/reservation/ReservationResponse.java
+++ src/main/java/kr/hhplus/be/server/interfaces/reservation/ReservationResponse.java
@@ -1,10 +1,24 @@
 package kr.hhplus.be.server.interfaces.reservation;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import kr.hhplus.be.server.domain.reservation.ReservationInfo;
+
+import java.time.LocalDateTime;
 
 public record ReservationResponse(
-        @Schema(description = "예약 상태", example = "reserved")
-        String status,
+        @Schema(description = "userId", example = "12L")
+        String userId,
         @Schema(description = "유효 시간(분)", example = "5")
-        int validMinutes
-) {}
+        LocalDateTime validMinutes
+) {
+        public static ReservationResponse of(ReservationInfo info) {
+                return new ReservationResponse("reserved", LocalDateTime.now()); // 유효시간은 정책에 따라 상수로 처리
+        }
+
+        public static ReservationResponse from(ReservationInfo info) {
+                return new ReservationResponse(
+                        info.seatStatus(),
+                        info.reservedAt()
+                );
+        }
+}
