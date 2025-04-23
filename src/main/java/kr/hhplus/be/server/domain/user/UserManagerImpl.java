5641dd882c754c4643439f9471534574ed5e44dd
westkitekim <westkite418@gmail.com>
2025-04-23 23:14:16 +0900

    style: 미사용 로직 제거

--- src/main/java/kr/hhplus/be/server/domain/user/UserManagerImpl.java
+++ src/main/java/kr/hhplus/be/server/domain/user/UserManagerImpl.java
@@ -2,7 +2,6 @@ package kr.hhplus.be.server.domain.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Component
 @RequiredArgsConstructor
